a24046eaa6ce539645aec0ea067b01fc1270ce2c
PEDRO HENRIQUE VIEIRA DE OLIVEIRA <1300811@redenotredame.local>
2025-11-12 13:30:09 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <div className="text-4xl text-zinc-800 text-center">KanbanBoard</div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/KanbanBoard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { DragDropContext } from "react-beautiful-dnd";
+
+function KanbanBoard() {
+  <></>;
+}
+
+export default KanbanBoard;
